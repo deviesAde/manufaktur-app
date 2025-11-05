c533a56c4687c6694dc0a1cc4ffe384953ae83d0
Devies Ade <deviesade@gmail.com>
2025-11-05 20:11:55 +0700

    Refactor boot method in AppServiceProvider to enforce HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,10 +18,16 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-   public function boot(): void
+ public function boot()
 {
-    if (config('app.env') === 'production') {
-        URL::forceScheme('https');
+    // Force HTTPS selalu, regardless of environment
+    URL::forceScheme('https');
+
+    if($this->app->environment('production')) {
+        $this->app['request']->setTrustedProxies(
+            ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16'],
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR
+        );
     }
 }
 }
