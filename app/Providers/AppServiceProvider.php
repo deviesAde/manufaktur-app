a8b911b8b00f983fe3639f8e86c610cddf6bb785
Devies Ade <deviesade@gmail.com>
2025-11-27 17:45:05 +0700

    fix2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
 public function boot()
 {
-    // Force HTTPS selalu, regardless of environment
+  
     URL::forceScheme('https');
 
     if($this->app->environment('production')) {
