4525a671f7e7570fc8f039a2c162aa42ea57a4e5
Devies Ade <deviesade@gmail.com>
2025-11-27 23:24:25 +0700

    fix: Filament login 403 and mixed  updating User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -51,5 +53,8 @@ class User extends Authenticatable
 {
     return $this->name ?: 'User';
 }
-
+public function canAccessPanel(Panel $panel): bool
+    {
+       return true;
+    }
 }
